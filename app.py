509478a8920a94310a100bed2967bc73b0ad6775
Josh Trujillo <josh.trujillo0@gmail.com>
2024-11-02 22:20:49 -0600

    Added Response.send implementation.

--- app.py
+++ app.py
@@ -62,7 +62,26 @@ class Response:
         """
         Write this response to a socket.
         """
-        raise NotImplementedError
+        content_length = self.headers.get("content-length")
+        if content_length is None:
+            try:
+                body_stat = os.fstat(self.body.fileno())
+                content_length = body_stat.st_size
+            except OSError:
+                self.body.seek(0, os.SEEK_END)
+                content_length = self.body.tell()
+                self.body.seek(0, os.SEEK_SET)
+
+            if content_length > 0:
+                self.headers.add("content-length", content_length)
+
+        headers = b"HTTP/1.1 " + self.status + b"\r\n"
+        for header_name, header_value in self.headers:
+            headers += f"{header_name}: {header_value}\r\n".encode()
+
+        sock.sendall(headers + b"\r\n")
+        if content_length > 0:
+            sock.sendfile(self.body)
 
 
 def serve_file(sock: socket.socket, path: str) -> None:
