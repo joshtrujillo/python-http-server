80d8808cb39ae0930f66a31e88d1e934fbe0d1ba
Josh Trujillo <josh.trujillo0@gmail.com>
2024-11-02 19:07:53 -0600

    Added serve_files() function.

--- app.py
+++ app.py
@@ -4,8 +4,57 @@ A simple python http server.
 @author Josh Trujillo
 """
 
+import mimetypes
+import os
+from os.path import abspath
 import socket
 import typing
+
+SERVER_ROOT = os.path.abspath("www")
+
+FILE_RESPONSE_TEMPLATE = """\
+HTTP/1.1 200 OK
+Content-type: {content_type}
+Content-length: {content_length}
+
+""".replace(
+    "\n", "\r\n"
+)
+
+
+def serve_file(sock: socket.socket, path: str) -> None:
+    """
+    Given a socket and the relative path to a file (relative to
+    SERVER_SOCK), send that file to the socket if it exists. If the file doesn't exist, sent a "404 Not Found" response.
+    """
+    if path == "/":
+        path = "/index.html"
+
+    abspath = os.path.normpath(os.path.join(SERVER_ROOT, path.lstrip("/")))
+    if not abspath.startswith(SERVER_ROOT):
+        sock.sendall(NOT_FOUND_RESPONSE)
+        return
+
+    try:
+        with open(abspath, "rb") as f:
+            stat = os.fstat(f.fileno())
+            content_type, encoding = mimetypes.guess_type(abspath)
+            if content_type is None:
+                content_type = "application/octet-stream"
+
+            if encoding is not None:
+                content_type += f"; charset={encoding}"
+
+            response_headers = FILE_RESPONSE_TEMPLATE.format(
+                content_type=content_type,
+                content_length=stat.st_size,
+            ).encode("ascii")
+
+            sock.sendall(response_headers)
+            sock.sendfile(f)
+    except FileNotFoundError:
+        sock.sendall(NOT_FOUND_RESPONSE)
+        return
 
 
 class Request(typing.NamedTuple):
@@ -131,8 +180,11 @@ with socket.socket() as server_sock:
         with client_sock:
             try:
                 request = Request.from_socket(client_sock)
-                print(request)
-                client_sock.sendall(NOT_FOUND_RESPONSE)
+                if request.method != "GET":
+                    client_sock.sendall(NOT_FOUND_RESPONSE)
+                    continue
+
+                serve_file(client_sock, request.path)
             except Exception as e:
                 print(f"Failed to parse request: {e}")
                 client_sock.sendall(BAD_REQUEST_RESPONSE)
